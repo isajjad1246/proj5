a83acfebb45d176334598f6cb7e167ca5ee6d722
Reiya Dave <reiya.dave@gmail.com>
2022-12-07 14:20:47 -0500

    reiya

---
+++ app/src/main/java/com/example/proj5/ChicagoStyleActivity.java
@@ -0,0 +1,14 @@
+package com.example.proj5;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ChicagoStyleActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chicago_style);
+    }
+}
